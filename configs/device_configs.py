736a888d1db81c77fcf30c09d526f1eaaffe9a2c
Khrystyna <khrystynavysotska@gmail.com>
2021-11-16 02:56:57 +0200

    Made use of environment variables

--- configs/device_configs.py
+++ configs/device_configs.py
@@ -1,9 +1,5 @@
 import os
 
-# DEVICE_ID = os.environ.get('DEVICE_ID')
-# PRIVATE_KEY_FILE_PATH = "./keys/rsa_private.pem"
-# PUBLISHING_FREQUENCY_IN_SECONDS = os.environ.get('PUBLISHING_FREQUENCY_IN_SECONDS')
-
-DEVICE_ID = "device-3"
+DEVICE_ID = os.environ.get('DEVICE_ID')
 PRIVATE_KEY_FILE_PATH = "./keys/rsa_private.pem"
-PUBLISHING_FREQUENCY_IN_SECONDS = 0.1
+PUBLISHING_FREQUENCY_IN_SECONDS = os.environ.get('PUBLISHING_FREQUENCY_IN_SECONDS')
--- configs/gcp_configs.py
+++ configs/gcp_configs.py
@@ -1,12 +1,8 @@
 import os
 
-# REGION = os.environ.get('REGION')
-# PROJECT_ID = os.environ.get('PROJECT_ID')
-# REGISTRY_ID = os.environ.get('REGISTRY_ID')
-
-REGION = "europe-west1"
-PROJECT_ID = "cloud-lab-3-332215"
-REGISTRY_ID = "float-sensors-registry"
+REGION = os.environ.get('REGION')
+PROJECT_ID = os.environ.get('PROJECT_ID')
+REGISTRY_ID = os.environ.get('REGISTRY_ID')
 
 DEFAULT_TOPIC = "events"
 TELEMETRY_TOPIC = "telemetry"
